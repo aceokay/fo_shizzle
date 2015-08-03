5a07a27f02b5c344f570598a6289ca1adf713bdb
Michael Summers & Austin Kincaid <student@epicodus.com>
2015-08-03 13:43:39 -0700

    Initial commit

---
+++ spec/fo_shizzle_spec.rb
@@ -0,0 +1,9 @@
+require('rspec')
+require('fo_shizzle')
+require('pry')
+
+describe('String#foshizzle') do
+    it('returns a string as is when no "s" ') do
+        #expect("".foshizzle).to(eq(""))
+    end
+end
