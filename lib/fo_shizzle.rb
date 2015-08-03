c3189c3a5fc1c383eee335247b302c2403056195
Michael Summers & Austin Kincaid <student@epicodus.com>
2015-08-03 14:40:06 -0700

    build spec to test and made method to turn an 's' into a 'z'

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -1,7 +1,14 @@
 class String
   define_method(:fo_shizzle) do
-    split_sentence = self.split
-    #split_sentence.each do |word|
-    split_sentence.join(" ")
+    split_sentence = self.split("")
+    word_split = []
+    split_sentence.each() do |letter|
+      if(letter === "s")
+        word_split.push("z")
+      else
+        word_split.push(letter)
+      end
+    end
+    word_split.join("")
   end
 end
--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -6,4 +6,8 @@ describe('String#fo_shizzle') do
   it('returns a string as is when no "s" ') do
     expect("onion".fo_shizzle).to(eq("onion"))
   end
+
+  it('replaces every "s" in a string with a "z"') do
+    expect("onions".fo_shizzle).to(eq("onionz"))
+  end
 end
