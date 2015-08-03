70ac522019c55bca47e688ed584c47a8d3096a87
Michael Summers & Austin Kincaid <student@epicodus.com>
2015-08-03 14:59:11 -0700

    build spec to test and make method make sure to not turn a first letter's' into a 'z'

--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -14,4 +14,8 @@ describe('String#fo_shizzle') do
   it('does NOT replace the first letter when it is an s') do
     expect("sunshine".fo_shizzle).to(eq("sunzhine"))
   end
+
+  it('replaces letters correctly in a string of words') do
+    expect("moses supposes his toeses are roses".fo_shizzle)).to(eq("mozez suppozez hiz toezez are rozez"))
+  end
 end
