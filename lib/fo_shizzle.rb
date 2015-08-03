a5e6d55f8e4c99496eb10d7c51608a37d9ebd477
Michael Summers & Austin Kincaid <student@epicodus.com>
2015-08-03 14:58:39 -0700

    build spec to test and make method make sure to not turn a first letter's' into a 'z'

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -2,10 +2,20 @@ class String
   define_method(:fo_shizzle) do
     split_sentence = self.split("")
     word_split = []
+    previousSpace = true
     split_sentence.each() do |letter|
-      if(letter === "s")
-        word_split.push("z")
+      if(letter === " ")
+        previousSpace = true
+      elsif(letter === "s")
+        if (previousSpace === true)
+          previousSpace = false
+          word_split.push(letter)
+        else
+          previousSpace = false
+          word_split.push("z")
+        end
       else
+        previousSpace = false
         word_split.push(letter)
       end
     end
--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -10,4 +10,8 @@ describe('String#fo_shizzle') do
   it('replaces every "s" in a string with a "z"') do
     expect("onions".fo_shizzle).to(eq("onionz"))
   end
+
+  it('does NOT replace the first letter when it is an s') do
+    expect("sunshine".fo_shizzle).to(eq("sunzhine"))
+  end
 end
